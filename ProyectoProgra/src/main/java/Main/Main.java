75ff8a1174c5f2fb4f1dc153141dcaa5e0a2fe4d
2LealFlores9 <114177211+2LealFlores9@users.noreply.github.com>
2022-11-19 13:56:07 -0600

    Update Main.java

--- ProyectoProgra/src/main/java/Main/Main.java
+++ ProyectoProgra/src/main/java/Main/Main.java
@@ -7,5 +7,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException{
         InicioSesion inicioSesion = new InicioSesion(GestorUsuarios.leerUsuarios());
+        //esto es una prueba de git
     }
 }
