f722034d776776e08f721d000950569f8cb5698c
Milagro25 <113941104+Milagro25@users.noreply.github.com>
2022-12-09 16:38:04 -0600

    Agrego de TXT

--- ProyectoProgra/src/main/java/GUI/Facturas.java
+++ ProyectoProgra/src/main/java/GUI/Facturas.java
@@ -1,33 +1,50 @@
 package GUI;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.util.Scanner;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class Facturas extends JFrame{
-    public Facturas(){
+public class Facturas extends JFrame {
+
+    public Facturas(String[][] Factura) {
+
+        //Creación de la ventana
         VENTANA.setDefaultCloseOperation(EXIT_ON_CLOSE);
         VENTANA.setResizable(false);
         VENTANA.setLocation(550, 100);
         VENTANA.setTitle("Generar Factura");
         VENTANA.setSize(620, 480);
         VENTANA.setVisible(true);
+
+        IMAGEN_FONDO.setIcon(IMAGEN);
+        IMAGEN_FONDO.setSize(700, 700);
+
+        TXT_CLIENTE.setFont(new Font("Serif", 3, 18));
+        TXT_CLIENTE.setText("Nombre de Cliente :");
+        TXT_CLIENTE.setBounds(50, 70, 150, 40);
+        TXT_CLIENTE.setForeground(new Color(255, 255, 255));
+
+        TXT_ID.setFont(new Font("Serif", 3, 18));
+        TXT_ID.setText("Identificación :");
+        TXT_ID.setBounds(50, 120, 150, 40);
+        TXT_ID.setForeground(new Color(255, 255, 255));
+
+
     }
     //Crear factura y de clarar variables.
     //Prueba.
-    private final JFrame VENTANA = new JFrame();
     
-    public static void main(String[] args) {
-    Scanner Prod =  new Scanner(System.in); 
-    String nombre = " "; 
-    double Precio; 
-    int Cantidad;
-    double SubTotal; 
-    double Total; 
-    double IGV = 0.18; 
-    double descuento; 
-
-    JOptionPane.showInputDialog(" "); 
-    nombre=Prod.next();
-    }
+    private final String RUTA_IMAGEN = "src\\main\\java\\Imagenes\\FondoUno.jpg";
+    
+    private final JFrame VENTANA = new JFrame();
+    private final ImageIcon IMAGEN = new ImageIcon(RUTA_IMAGEN);
+    private final JLabel IMAGEN_FONDO = new JLabel();
+    
+    private final JLabel TXT_CLIENTE = new JLabel();
+    private final JLabel TXT_ID = new JLabel();
+    
 }
